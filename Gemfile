0758b0120fa2d045dc2be05385027a96de089af6
sergiomauz <sergio.mauz88@gmail.com>
2020-11-07 17:30:36 -0500

    Removed Users migration

--- Gemfile
+++ Gemfile
@@ -37,3 +37,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Sessions
+gem 'devise'
+gem 'doorkeeper'
--- db/schema.rb
+++ db/schema.rb
@@ -24,14 +24,4 @@ ActiveRecord::Schema.define(version: 2020_11_05_160842) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
