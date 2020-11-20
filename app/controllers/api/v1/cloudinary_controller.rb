53047ffaeb6c6df5c56bf0a84173e860be17cfcc
sergiomauz <sergio.mauz88@gmail.com>
2020-11-20 03:49:59 -0500

    Changed cloudinary controller

--- app/controllers/api/v1/cloudinary_controller.rb
+++ app/controllers/api/v1/cloudinary_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::CloudinaryController < ApplicationController
   # GET /cloudinary
   def index
-    @upload_preset = { 'cloud_name' => 'sergiouce16', 'upload_preset' => '5tf4a2fgjtvqsjk7bh49zybdbw6tjhze' }
+    @upload_preset = { 'cloud_name' => 'mt4yammy58c7cu2r5u9u8acwuqeyuy79', 'upload_preset' => '2q4gaah5pzg6bpany8j6pmjcxy5amqfcjq976ce93rfttupa65drd8j4qdjzgmy4' }
     render json: @upload_preset
   end
 end
